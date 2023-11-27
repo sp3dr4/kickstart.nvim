463946a5e468c935bbc5af3ea31063a9f58e0392
Simone Pedrazzi <pedra89@gmail.com>
2023-11-27 22:06:26 +0100

    uncomment mason useful languages

--- init.lua
+++ init.lua
@@ -512,11 +512,11 @@ require('mason-lspconfig').setup()
 --  define the property 'filetypes' to the map in question.
 local servers = {
   -- clangd = {},
-  -- gopls = {},
-  -- pyright = {},
+  gopls = {},
+  pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  html = { filetypes = { 'html', 'twig', 'hbs'} },
 
   lua_ls = {
     Lua = {
